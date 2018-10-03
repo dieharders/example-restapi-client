9855de0da598aa7d122fc333058081fc502d2208
Sorob Raissi <cybrobo@gmail.com>
2018-10-02 21:42:08 -0700

    cahnged bg color

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular6-httpclient!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to example-restapi-client!');
   }));
 });
