dc247acbf00dd0856ef4311130175e835ef0980c
Sorob Raissi <cybrobo@gmail.com>
2018-10-03 00:08:57 -0700

    added /dist back to gitignore. Added server.js to serve our /dist folder satatic files.

---
+++ server.js
@@ -0,0 +1,18 @@
+//** For Heroku to serve us our Angular App **//
+
+// Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files from the dist/ directory
+app.use(express.static('./dist/example-restapi-client'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/example-restapi-client/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
